f7d7ea638ac76ec68c4f580f6557d010bdd2fac9
monish <monishoffcl04@gmail.com>
2025-07-19 22:09:38 +0530

    week 5

---
+++ week-5_MicroservicesWithSpringBootAndSpringCloud/2.MicroservicesWithApiGateWay/CreatingMicroServicesForLoanAndAccount/account/account/src/main/java/com/example/account/controller/AccountController.java
@@ -0,0 +1,21 @@
+package com.example.account.controller;
+
+import java.util.Map;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class AccountController {
+
+    @GetMapping("/accounts/{number}")
+    public Map<String, Object> getAccountDetails(@PathVariable String number) {
+        return Map.of(
+            "number", number,
+            "type", "savings",
+            "balance", 234343
+        );
+    }
+}
+
